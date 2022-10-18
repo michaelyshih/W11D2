c77ad5bfaffb693e0978d19f9e049306442d2780
shihyulung <shihyulung@yahoo.com.tw>
2022-10-18 13:54:09 -0700

    add coffeebeans

---
+++ practice-for-week-14-react-context-review-with-tests-long-practice/src/components/SelectedCoffeeBean.js
@@ -0,0 +1,14 @@
+import { useCoffeeContext } from "../context/CoffeeContext";
+
+const SelectedCoffeeBean = () => {
+
+  const {coffeeBean} = useCoffeeContext();
+  return(
+    <div className="selected-coffee">
+      <h2>{coffeeBean.name}</h2>
+    </div>
+
+  )
+}
+
+export default SelectedCoffeeBean;
