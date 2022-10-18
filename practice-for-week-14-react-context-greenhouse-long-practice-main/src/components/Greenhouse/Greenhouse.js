9656b43863fb637c89fcce89b38f003e1a1b8cbf
shihyulung <shihyulung@yahoo.com.tw>
2022-10-18 14:37:05 -0700

    finish phase 2

---
+++ practice-for-week-14-react-context-greenhouse-long-practice-main/src/components/Greenhouse/Greenhouse.js
@@ -0,0 +1,23 @@
+import dayImage from './images/greenhouse-day.jpg';
+import nightImage from './images/greenhouse-night.jpg';
+import './Greenhouse.css';
+import { useTheme } from '../../context/ThemeContext';
+
+import LightSwitch from './LightSwitch';
+import ClimateStats from './ClimateStats';
+
+function Greenhouse() {
+  const {themeName, setThemeName} = useTheme();
+  return (
+    <section>
+      <img  className='greenhouse-img'
+            src={themeName === "day" ? dayImage : nightImage}
+            alt='greenhouse'
+      />
+      <LightSwitch />
+      <ClimateStats />
+    </section>
+  );
+}
+
+export default Greenhouse;
--- practice-for-week-14-react-context-review-with-tests-long-practice/src/components/SetCoffeeBean.js
+++ practice-for-week-14-react-context-review-with-tests-long-practice/src/components/SetCoffeeBean.js
@@ -9,13 +9,15 @@ const SetCoffeeBean = ({ coffeeBeans }) => {
       <h2>Select a Coffee Bean</h2>
       <select
         name="coffee-bean"
+        onChange={(e) => setCoffeeBeanId(e.target.value)}
+        // onChange={(e) => console.log(e.target.value)}
         value={coffeeBean.id}
+
       >
         {coffeeBeans.map(bean => (
           <option
             key={bean.id}
             value={bean.id}
-            onChange={() => setCoffeeBeanId(bean.id)}
           >
             {bean.name}
           </option>
